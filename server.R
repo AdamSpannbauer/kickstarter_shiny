e0b9f02d9916f2f2362469f5da492f15dc8c6970
JNKirshner <98779332+JNKirshner@users.noreply.github.com>
2022-02-08 08:54:41 -0500

    Added Date Range filter

--- server.R
+++ server.R
@@ -4,14 +4,17 @@ library(fpp3)
 server <- function(input, output, session) {
   plot_df <- reactive({
     # TODO: add ability to filter by date
-
+    min_date <- input$selected_date_range[1]
+    max_date<- input$selected_date_range[2]
+    filtered_df <- ks_df[
+      ks_df$launched_week >= min_date <= max_date, ]
     # TODO: add ability to filter by state
 
     # TODO: add ability to filter by main_category
 
     #------------------------
     ks_df_agg <- aggregate(
-      usd_pledged ~ deadline_week + main_category, ks_df, sum
+      usd_pledged ~ deadline_week + main_category,filtered_df, sum
     )
 
 
--- ui.R
+++ ui.R
@@ -4,6 +4,12 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       # TODO: add ability to filter by date
+      dateRangeInput(inputId = 'selected_date_range',
+                     label = 'Select Date range',
+                     start = min(ks_df$launched_week),
+                     end= max(ks_df$launched_week) ,
+                     min= min(ks_df$launched_week) ,
+                     max= max(ks_df$launched_week))
 
       # TODO: add ability to filter by state
 
